d4842ce7c0c6bc215e2c94a5d5adfd20a74f2509
LeSeulMano <marin.clousier@gmail.com>
2023-12-14 23:02:05 +0100

    1.0.3

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-
+import Vue from 'vue';
 import VueCookies from 'vue-cookies';
 
 import router from './router'
 
+Vue.config.productionTip = false
 
 
 createApp(App).use(VueCookies).use(router).mount('#app')
